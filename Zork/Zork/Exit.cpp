dbc8c1a6408a4734eca406c1e276a10ec7b46083
juancacuque <juancarnaves@gmail.com>
2026-07-15 13:49:33 +0200

    Exit
    Exit class added, with lock and key

---
+++ Zork/Zork/Exit.cpp
@@ -0,0 +1,34 @@
+#include "Exit.h"
+#include <iostream>
+
+Exit::Exit(const std::string& name, const std::string& description, ExitDirection direction, Room* source, Room* destination, bool lock): Entity(EntityType::Exit, name, description), direction(direction), source(source), destination(destination), key(nullptr), lock(lock)
+{
+}
+void Exit::Update()
+{
+
+}
+void Exit::SetKey(Item* key)
+{
+	this->key = key;
+}
+void Exit::Unlock()
+{
+	lock = false;
+}
+
+bool Exit::GetLockStatus() const
+{
+	return lock;
+}
+
+
+Room* Exit::GetDestination() const
+{
+	return destination;
+}
+
+ExitDirection Exit::GetDirection() const
+{
+	return direction;
+}
---
+++ Zork/Zork/Exit.h
@@ -0,0 +1,34 @@
+#pragma once
+#include "Entity.h"
+
+class Room;
+class Item;
+
+enum class ExitDirection
+{
+	North,
+	South,
+	East,
+	West
+};
+
+class Exit : public Entity
+{
+public:
+	Exit(const std::string& name, const std::string& description, ExitDirection direction, Room* source, Room* destination, bool lock);
+	void Update() override;
+	void SetKey(Item* key);
+	void Unlock();
+
+	Room* GetDestination() const;
+	ExitDirection GetDirection() const;
+	bool GetLockStatus() const;
+
+private:
+	ExitDirection direction;
+	Room* source;
+	Room* destination;
+	Item* key;
+
+	bool lock;
+};
